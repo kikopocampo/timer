74c3c30472e9dac74c9edda830eb31ed58163d66
Francisco <kikopocampo@gmail.com>
2022-10-25 13:02:32 -0600

    edited if statament so it works at 0 secs as well

--- timer1.js
+++ timer1.js
@@ -1,6 +1,6 @@
 const secs = process.argv.slice(2);
 for (const sec of secs) {
-  if (sec && sec > 0) {
+  if (sec && sec > -1) {
     setTimeout(() => {
       process.stdout.write('\x07');
     }, sec * 1000);
